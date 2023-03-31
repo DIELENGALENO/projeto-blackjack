3ca922048b6895a2b4c5cf7006dd87b7fd1eedf2
DIELEN <dielen_lemos@hotmail.com>
2023-03-31 18:53:39 +0200

    Nosso Jogo

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,65 @@
  * 
  * 
  */
+console.log("Boas vindas ao jogo de BlackJack!")
+
+if (confirm ("Quer iniciar uma nova rodada? ")) {
+
+// Sortear cartas para cada jogador.
+
+const cartaUsuario = comprarCarta();
+const cartaUsuario2 = comprarCarta();
+const cartaComputador = comprarCarta()
+const cartaComputador2 = comprarCarta()
+
+
+
+//Imprimir cartas e pontuação de cada jogador.
+
+const pontuacaoUsuario = cartaUsuario.valor + cartaUsuario2.valor
+const pontuacaoComputador = cartaComputador.valor + cartaComputador2.valor
+console.log(`Usuário - cartas: ${cartaUsuario.texto} ${cartaUsuario2.texto} - pontuação ${pontuacaoUsuario}`)
+console.log(`Computador - cartas: ${cartaComputador.texto} ${cartaComputador2.texto} - pontuação ${pontuacaoComputador}`)
+
+
+//Imprimir vencedor ou empate.
+
+
+
+
+if (pontuacaoUsuario > pontuacaoComputador ) {
+
+   console.log("O usuário ganhou!")
+} else if (pontuacaoComputador > pontuacaoUsuario) {
+
+   console.log("O computador ganhou!")
+
+} else {
+
+   console.log("Empate!")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} else { 
+
+   console.log("O jogo acabou.")
+
+}
